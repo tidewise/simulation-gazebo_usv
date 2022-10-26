b1e0f3c2e0ce084ddf53a84487437d4108ccdfe4
kapeps <rodrigo.kappes@student-cs.fr>
2022-10-26 10:41:34 -0300

    fix: changed loop and formating issues

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -4,15 +4,14 @@
 
 using namespace gazebo_usv;
 
-sdf::ElementPtr utilities::findPluginElement(
-    sdf::ElementPtr enclosing, std::string const& file_name
-) {
+sdf::ElementPtr utilities::findPluginElement(sdf::ElementPtr enclosing,
+    std::string const& file_name)
+{
     sdf::ElementPtr plugin_element = enclosing->GetElement("plugin");
-    while (plugin_element)
-    {
+    while (plugin_element) {
         if (plugin_element->Get<std::string>("filename") == file_name) {
-            gzmsg << "Found plugin: " << plugin_element->Get<std::string>("name")
-                  << " (" << file_name << ")" << std::endl;
+            gzmsg << "Found plugin: " << plugin_element->Get<std::string>("name") << " ("
+                  << file_name << ")" << std::endl;
             return plugin_element;
         }
         plugin_element = plugin_element->GetNextElement("plugin");
@@ -20,15 +19,14 @@ sdf::ElementPtr utilities::findPluginElement(
     return sdf::ElementPtr();
 }
 
-sdf::ElementPtr utilities::findPluginElementByName(
-    sdf::ElementPtr enclosing, std::string const& plugin_name
-) {
+sdf::ElementPtr utilities::findPluginElementByName(sdf::ElementPtr enclosing,
+    std::string const& plugin_name)
+{
     sdf::ElementPtr plugin_element = enclosing->GetElement("plugin");
-    while (plugin_element)
-    {
+    while (plugin_element) {
         if (plugin_element->Get<std::string>("name") == plugin_name) {
-            gzmsg << "Found plugin: " << plugin_element->Get<std::string>("name")
-                  << " (" << plugin_name << ")" << std::endl;
+            gzmsg << "Found plugin: " << plugin_element->Get<std::string>("name") << " ("
+                  << plugin_name << ")" << std::endl;
             return plugin_element;
         }
         plugin_element = plugin_element->GetNextElement("plugin");
@@ -36,9 +34,9 @@ sdf::ElementPtr utilities::findPluginElementByName(
     return sdf::ElementPtr();
 }
 
-sdf::ElementPtr utilities::getPluginElement(
-    sdf::ElementPtr enclosing, std::string const& file_name
-) {
+sdf::ElementPtr utilities::getPluginElement(sdf::ElementPtr enclosing,
+    std::string const& file_name)
+{
     auto element = findPluginElement(enclosing, file_name);
 
     if (!element) {
@@ -53,9 +51,9 @@ sdf::ElementPtr utilities::getPluginElement(
     return element;
 }
 
-sdf::ElementPtr utilities::getPluginElementByName(
-    sdf::ElementPtr enclosing, std::string const& plugin_name
-) {
+sdf::ElementPtr utilities::getPluginElementByName(sdf::ElementPtr enclosing,
+    std::string const& plugin_name)
+{
     auto element = findPluginElementByName(enclosing, plugin_name);
 
     if (!element) {
@@ -71,27 +69,28 @@ std::string utilities::getNamespaceFromPluginName(std::string const& plugin_name
 {
     return std::regex_replace(plugin_name, std::regex("__"), "/");
 }
-gazebo::physics::LinkPtr utilities::getLinkFromName(gazebo::physics::ModelPtr model, std::string const& link_name, std::string const& plugin_name)
+gazebo::physics::LinkPtr utilities::getLinkFromName(gazebo::physics::ModelPtr model,
+    std::string const& link_name,
+    std::string const& plugin_name)
 {
     std::string link_name_iterated = link_name;
     std::string plugin_name_iterated = plugin_name.substr(0, plugin_name.rfind("__"));
     std::size_t pos = plugin_name_iterated.rfind("__");
-    std::string plugin_prefix = plugin_name_iterated.substr(pos +2);
+    std::string plugin_prefix = plugin_name_iterated.substr(pos + 2);
     auto link = model->GetLink(link_name_iterated);
 
-    do
-    {
+    while (pos != std::string::npos) {
         if (!link) {
             pos = plugin_name_iterated.rfind("__");
-            plugin_prefix = plugin_name_iterated.substr(pos +2);
+            plugin_prefix = plugin_name_iterated.substr(pos + 2);
             plugin_name_iterated = plugin_name_iterated.substr(0, pos);
-            link_name_iterated=plugin_prefix+"::"+link_name_iterated;
-        }else{
+            link_name_iterated = plugin_prefix + "::" + link_name_iterated;
+        }
+        else {
             break;
         }
         link = model->GetLink(link_name_iterated);
-    } while (pos != std::string::npos);
-
+    }
 
     return link;
 }
